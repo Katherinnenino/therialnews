7cb7f748ab1a1507f38b662dd976cae90247b678
Katherinne Niño <kattyvaleninovillesca@gmail.com>
2023-07-17 17:24:47 +0000

    listo para desplegar

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
   has_many :posts
-  # has_many :comments
+  has_many :comments
+
   def authorize_request(kind = nil)
     unless kind.include?(current_user.role)
     redirect_to posts_path, notice: "Lo siento, no estas autorizado para realizar esta acción."
